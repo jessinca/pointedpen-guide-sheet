beb0c0dfb01049e466d25992f24cfa83937f9b9b
Jessinca Jong <jessinca.natali@gmail.com>
2016-12-23 11:30:30 +0800

    Remove hardcoding that I added because I wanted to write something on top of the guide sheet 🙈

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ const drawGuideSheet = (options) => {
 		xHeight,
 		xColor
 	} = options;
-	let y = pageMargin + 15;
+	let y = pageMargin;
 	const pageMarginx = pageMargin;
 	
 	const maxHorizontalLineY = pageHeight-pageMargin;
